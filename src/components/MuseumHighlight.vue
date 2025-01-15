87b0a6d318969de59ee29ce123e7e8e043dd52da
acute.developer <acute.developer@nigelmpeters.co.uk>
2025-01-15 18:17:41 +0000

    Feat: Creating project starting point

---
+++ src/components/MuseumHighlight.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="museum-highlight">
+        <!-- Display the available information for the highlight -->
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'MuseumHighlight',
+    components: {
+
+    },
+    mixins: [
+    ],
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+        newsDate() {
+            // Highlight's news item date
+        },
+    },
+    methods: {
+
+    },
+    created() {
+
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.museum-highlight {
+
+}
+</style>
